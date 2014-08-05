20c468b632fa69fc3355438f251deccffd011497
kennkong <kwolking@cfl.rr.com>
2014-08-04 23:27:53 -0400

    Working station select for current only

--- stations.js
+++ stations.js
@@ -1,19 +1,19 @@
 //stations
-var STATIONID=$.query.get('stationid');
 $(function () {
+	STATIONID=$.query.get('stationid');
+	if (STATIONID == "") STATIONID=55;
 	$("#stationdiv").prepend($("<form></form>").attr("id", "stations"));
 	$("#stations").prepend($("<select></select>").attr("id", "stationlist"));
 	$("#stationlist").attr("onchange", 
-		"STATIONID = $(':selected')[0].value; $('#selected').text(STATIONID)");
+//		"STATIONID = $(':selected')[0].value; $('#selected').text(STATIONID)");
+		"STATIONID = $(':selected')[0].value; location.search='?stationid='+STATIONID;");
 	$.getJSON('wsdata.php?sensor=stations',	function(data) {
 		$.each(data, function(i, station) {
 			var option = $("<option></option>")
 				.attr("value",station[0])
-				.text(station[1]);
-			if (i == 0) {
+				.text(station[0]+" - "+station[1]);
+			if (STATIONID == station[0]) {
 				option.attr("selected","selected");
-				STATIONID = station[0];
-				$("#selected").text(STATIONID);
 			}	
 			$("#stationlist").append(option);
 		});
