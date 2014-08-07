4f6587f642cc95f25a86b08097577814875b1183
kennkong <kwolking@cfl.rr.com>
2014-08-07 02:04:09 -0400

    Made stations query synchronous

--- stations.js
+++ stations.js
@@ -1,25 +1,40 @@
-//stations
+//stations with synchronous request
+STATIONID = "";
 $(function () {
-	STATIONID=$.query.get('stationid');
-	if (STATIONID == "") STATIONID=55;
+
+    var str = location.search.replace(/^[?#]/, ''); // Remove leading ? or #
+	str = str.replace(/[&;]$/, ''); // Remove trailing & or ;
+	if (str.length > 0) {
+		$.each(str.split("[&;]"), function(i,piece) {
+			var key = piece.split("=")[0] || "";
+			if (key == "stationid") STATIONID = piece.split("=")[1] || "";
+		});
+	};
 	var scnt = 0;
 	$("#stationdiv").hide();
 	$("#stationdiv").prepend($("<form></form>").attr("id", "stations"));
 	$("#stations").prepend($("<select></select>").attr("id", "stationlist"));
 	$("#stationlist").attr("onchange", 
-//		"STATIONID = $(':selected')[0].value; $('#selected').text(STATIONID)");
 		"STATIONID = $(':selected')[0].value; location.search='?stationid='+STATIONID;");
-	$.getJSON('wsdata.php?sensor=stations',	function(data) {
-		$.each(data, function(i, station) {
-			var option = $("<option></option>")
-				.attr("value",station[0])
-				.text(station[0]+" - "+station[1]);
-			if (STATIONID == station[0]) {
-				option.attr("selected","selected");
-			}	
-			$("#stationlist").append(option);
-			scnt = i;
+	$.ajax( {
+		url: 'wsdata.php?sensor=stations',
+		async: false,
+		dataType: "json",
+		success: function(data) {
+			var scnt = 0;
+			console.log(data);
+			$.each(data, function(i, station) {
+				var option = $("<option></option>")
+					.attr("value",station[0])
+					.text(station[0]+" - "+station[1]);
+				if (STATIONID == "") STATIONID = station[0];
+				if (STATIONID == station[0]) {
+					option.attr("selected","selected");
+				}	
+				$("#stationlist").append(option);
+				scnt = i;
+			});
+			if (scnt > 0) $("#stationdiv").show();
+			}
 		});
-		if (scnt > 0) $("#stationdiv").show();
-	});
 });
