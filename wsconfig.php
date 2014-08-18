0059ea9d4ede2e82cddf0ddc0db65bd5223f77fd
kennkong <kwolking@cfl.rr.com>
2014-08-18 00:20:13 -0400

    Added configuration pages

---
+++ wsconfig.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+error_reporting(E_ALL);
+$stationid = explode("=", $_SERVER['QUERY_STRING'])[1];
+//print_r($_POST);
+$sql = "UPDATE stations SET ";
+$output_flags = 0;
+foreach ($_POST as $column => $value) {
+	if ($column == "submit") continue;
+	if ($column == "soutput_flags") {
+		foreach ($value as $flag => $fval) {
+			$output_flags += $fval;
+		}
+	} else {
+		$sql.=substr($column, 1)." = '".$value."', ";
+	}
+}
+$sql.="output_flags = $output_flags WHERE id = $stationid";
+//echo $sql."<br>";
+try {
+	$wdb = new PDO('mysql:dbname=weather');
+	$count = $wdb->exec($sql);
+//	echo "$count rows updated with message ".$wdb->errorInfo()[2]."<br>";
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
+echo "success";
+?>
+</body>
+</html>
